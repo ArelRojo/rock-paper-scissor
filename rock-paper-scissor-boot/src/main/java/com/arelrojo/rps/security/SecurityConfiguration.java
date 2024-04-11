0aee3f85fbc192f26ef996897e1e2fdccb205090
lisasaliasi <lisasaliasi@gmail.com>
2024-04-11 13:32:59 +0200

    Reformat

--- rock-paper-scissor-boot/src/main/java/com/arelrojo/rps/security/SecurityConfiguration.java
+++ rock-paper-scissor-boot/src/main/java/com/arelrojo/rps/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -24,26 +25,21 @@ public class SecurityConfiguration {
     private String password;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .csrf(c-> c.disable())
-                .httpBasic(Customizer.withDefaults());
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).csrf(AbstractHttpConfigurer::disable).httpBasic(Customizer.withDefaults());
         return http.build();
 
     }
 
     @Bean
-    UserDetailsService userDetailsService(){
+    UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername(username).password(password).roles().build());
         return manager;
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 }
